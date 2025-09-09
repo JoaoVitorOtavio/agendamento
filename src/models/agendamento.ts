8d5cbbdffe4bde17f7c2ff307fe11471d8ab9a35
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-09-09 17:22:27 -0300

    Configurando model

--- src/models/agendamento.ts
+++ src/models/agendamento.ts
@@ -1,1 +1,30 @@
-// TODO: model do agendamento
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+export type StatusAgendamento = "pendente" | "concluido" | "atrasado" | "cancelado";
+
+@Entity("agendamentos")
+export class Agendamento {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: "datetime" })
+    dataHora: Date;
+
+    @Column()
+    numeroContrato: string;
+
+    @Column()
+    motoristaNome: string;
+
+    @Column({ unique: true })
+    motoristaCpf: string;
+
+    @Column()
+    placaCaminhao: string;
+
+    @Column({
+        type: "text",
+        default: "pendente",
+    })
+    status: StatusAgendamento;
+}
