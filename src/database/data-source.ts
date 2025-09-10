aa3828fea9d6475b5acae308a9db09110b886694
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-09-09 21:10:04 -0300

    Adicionando verificacao na configuracao de datasource para executar banco in memory em caso de execucao de testes

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -2,11 +2,17 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import path from "path";
 
+const isTest = process.env.NODE_ENV === "test";
+
+const IN_MEMORY_DATABASE = ':memory:'
+
 export const AppDataSource = new DataSource({
     type: "sqlite",
-    database: path.join(__dirname, "agendamento-db.sqlite"),
-    synchronize: false,
-    migrations: [path.join(__dirname, "migrations", "*.ts")],
+    database: isTest
+        ? IN_MEMORY_DATABASE
+        : path.join(__dirname, "agendamento-db.sqlite"),
+    synchronize: isTest ? true : false,
     logging: false,
     entities: [path.join(__dirname, "../models/*.ts")],
+    migrations: [path.join(__dirname, "migrations", "*.ts")],
 });
